599d60b63c76c202fb573a41110b8268f19ec2a2
omid khatibi <omidkhatibiofficialy@gmail.com>
2024-12-02 12:40:07 +0330

    created half of Wallet secion

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,5 +1,58 @@
+import cartImg from "../assets/Card-Example.png";
+import { FaHistory } from "react-icons/fa";
+import { FaExchangeAlt } from "react-icons/fa";
+import { BsBoxArrowInUp } from "react-icons/bs";
+import { BsBoxArrowInDown } from "react-icons/bs";
+
 const Wallet = () => {
-  return <div>WALLET</div>;
+  return (
+    <div className="flex flex-col justify-between items-center w-full py-7">
+      <div className="flex flex-col md:flex-row w-full justify-center items-center md:gap-0 gap-5">
+        <div className="md:w-1/2 w-full flex justify-center items-center bg-green-500">
+          <img src={cartImg} alt="" />
+        </div>
+        <div className="md:w-1/2 w-full bg-red-500 flex justify-center items-center md:gap-7 gap-3">
+          <button className="hover-btn">
+            <a
+              href="#"
+              className="flex flex-col justify-center items-center bg-[#003366] text-white px-3 py-2 md:px-5 md:py-3 rounded-md"
+            >
+              <BsBoxArrowInUp className="text-xl md:text-2xl mb-2 rotate-icon" />
+              Recive
+            </a>
+          </button>
+          <button className="hover-btn">
+            <a
+              href="#"
+              className="flex flex-col  justify-center items-center bg-[#003366] text-white px-3 py-2 md:px-5 md:py-3 rounded-md"
+            >
+              <BsBoxArrowInDown className="text-xl md:text-2xl mb-2 rotate-icon" />
+              Send
+            </a>
+          </button>
+          <button className="hover-btn">
+            <a
+              href="#"
+              className="flex flex-col  justify-center items-center bg-[#003366] text-white px-3 py-2 md:px-5 md:py-3 rounded-md"
+            >
+              <FaExchangeAlt className="text-xl md:text-2xl mb-2 rotate-icon" />
+              Exchange
+            </a>
+          </button>
+          <button className="hover-btn">
+            <a
+              href="#"
+              className="flex flex-col  justify-center items-center bg-[#003366] text-white px-3 py-2 md:px-5 md:py-3 rounded-md"
+            >
+              <FaHistory className="text-xl md:text-2xl mb-2 rotate-icon" />
+              History
+            </a>
+          </button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Wallet;
